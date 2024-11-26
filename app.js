0b0c5b8f88dfe3bc1e3a5a65446e5b8ddd07355f
MinPhyoeMinThu <minphyoeminthu@acedatasystems.com>
2024-11-26 16:21:06 +0630

    [BUILT] Project Structure

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ dotenv.config();
 const app=express();
 
 app.use(express.json());
+app.use('/',(req,res)=>{
+    console.log("confirmed / .");
+    res.send("Visited / .");
+})
 
 const PORT=process.env.PORT || 3000;
 app.listen(PORT,()=>
